cc34f0c8e30e360119d78b6f2a0c0221c6ba530c
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-05 17:41:46 +0200

    esercizio completo all milestone

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,29 @@
-// Descrizione:
 // Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente 
+// ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente 
 // (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, 
+// ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, 
 // visualizzare nome e immagine di ogni contatto
+
+// Milestone 2
+// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra la conversazione del contatto cliccato
+
+// Milestone 3
+// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
+
+// Milestone 5 - opzionale
+// ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+// permette di cancellare il messaggio selezionato
+// ● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 
 var app = new Vue (
     {
@@ -16,6 +36,10 @@ var app = new Vue (
                 avatar: '_io',
                 newMessage: '',
             },
+
+            selectedContact: 0,
+
+            searchContact: '',
 
             contacts: [
                 {
@@ -186,6 +210,79 @@ var app = new Vue (
 
         methods: {
 
+            // Milestone 2
+            // funzione che al click seleziono il contatto
+            changeContact: function(i) { 
+                this.selectedContact = i
+            },
+
+            // Milestone 3
+            // inserici nuovo messaggio con risposta automatica
+            insertMessage: function() {
+                this.contacts[this.selectedContact].messages.push(
+                    {
+                        date: this.tellTime(),
+                        message: this.user.newMessage,
+                        status: 'sent'
+                    }
+                )
+                setTimeout(this.receiveAnswer, 3000);
+                this.user.newMessage = '' ;
+            },
+
+            // risposta automatica di cui sopra
+            receiveAnswer: function() { 
+                this.contacts[this.selectedContact].messages.push(
+                    {
+                        date: this.tellTime(),
+                        message: 'Ok',
+                        status: 'received'
+                    }
+                )
+            },
+
+            // calcolo data messaggio
+            tellTime: function() { 
+                const date = dayjs().format('DD/MM/YYYY HH:mm:ss')
+                return date
+            },
+
+            // Milestone 4
+            // filtra contatti con input
+            filterContacts: function(e) { 
+                let searchName = this.searchContact.toLowerCase();
+                let filterName = e.name.toLowerCase();
+                if (filterName.includes(searchName)) {
+                    return true
+                }
+            },
+
+            // Milestone 5
+            // apri dropdown menu
+            openMenu: function(i) { 
+                let menu = document.getElementsByClassName('message-menu')[i];
+                if (menu.classList.contains('active') === false) {
+                    menu.classList.add('active')
+                } else {
+                    menu.classList.remove('active')
+                }
+            },
+
+            // cancella messaggio
+            deleteMessage: function(i) { 
+                let menu = document.getElementsByClassName('message-menu')[i];
+                this.contacts[this.selectedContact].messages.splice(i,1)
+                menu.classList.remove('active')
+            },
+
+            // ultima connessione
+            lastConnection: function(i) { 
+                let lastIndex = this.contacts[i].messages.length - 1;
+                let last = this.contacts[i].messages[lastIndex];
+                if (last.status === 'received') {
+                    return last.date
+                }
+            }
 
         }
 
